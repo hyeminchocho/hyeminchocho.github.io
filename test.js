992f06ec6c1cdd6b4711a3180a188152375ca928
Hye Min Cho <3820319+hyeminchocho@users.noreply.github.com>
2019-06-04 16:24:03 -0700

    Init commit

---
+++ test.js
@@ -0,0 +1,7 @@
+// If you are using VanillaJS
+// const artyom = new Artyom();
+
+// Or if installed from NPM to use with a bundler
+import Artyom from "node_modules/artyom.js"
+// const artyom = require("artyom.js");
+const artyom = new Artyom();
